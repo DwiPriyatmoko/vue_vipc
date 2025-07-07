69588bf0a6bd4aef367ad5837da3f34debf5da0e
dwiPriyatmoko <dwipriyatmoko27@gmail.com>
2025-07-07 21:57:31 +0700

    Directives

--- src/components/MainButton.vue
+++ src/components/MainButton.vue
@@ -2,21 +2,25 @@
   // const buttonDisabled = true;
   const isLoading = false;
   const buttonAttributes = {
+    type: 'button',
     disabled: isLoading,
     class: 'text-white bg-blue-500 hover:bg-blue-700 font-bold py-2 px-4 rounded',
   };
   const renderIsLoading = () => {
     return isLoading ? 'Loading' : 'Submit';
   };
+  const url = {
+    attr: 'href',
+    link: '/home',
+  };
 </script>
 
 <template>
-  <!-- <button :disabled="buttonDisabled" class="btn">
-    <slot>BUTTON</slot>
-  </button> -->
-  <button v-bind="buttonAttributes" :id="`id-${buttonAttributes.class}`">
-    {{ renderIsLoading() }}
-  </button>
+  <a :[url.attr]="url.link">
+    <button v-bind="buttonAttributes" :id="`id-${buttonAttributes.class}`">
+      {{ renderIsLoading() }}
+    </button>
+  </a>
 </template>
 
 <style scoped></style>
