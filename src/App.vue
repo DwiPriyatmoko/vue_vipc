7dc634bfdde65db43915343468492b369c8f6f9a
dwiPriyatmoko <dwipriyatmoko27@gmail.com>
2025-07-07 14:36:04 +0700

    stage 2

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+  import MainButton from './components/MainButton.vue';
+  const name = 'Hellena';
+</script>
+
+<template>
+  <MainButton />
+  <h1>Vue + Vite</h1>
+  <p>{{ name }}</p>
+</template>
+
+<style scoped></style>
---
+++ src/components/MainButton.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <button class="btn">
+    <slot>BUTTON</slot>
+  </button>
+</template>
+
+<style scoped></style>
