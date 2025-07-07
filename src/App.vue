7b5c7df26a974a1106217d74bf8bc79b98514724
dwiPriyatmoko <dwipriyatmoko27@gmail.com>
2025-07-07 21:07:08 +0700

    Stage 5: Interpolation & Bindings

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <script setup>
   import MainButton from './components/MainButton.vue';
+  const greeting = 'Vue + Vite + Composition API';
+  const idBlog = 'blogPosts';
+  const blogClass = 'text-2xl font-bold text-blue-500';
 </script>
 
 <template>
   <MainButton />
   <h1>Vue + Vite</h1>
+
+  <div :id="idBlog" :class="blogClass">
+    {{ greeting }}
+  </div>
 
   <OptionsComponent />
   <CompositionComponent />
--- src/components/MainButton.vue
+++ src/components/MainButton.vue
@@ -1,8 +1,21 @@
-<script setup></script>
+<script setup>
+  // const buttonDisabled = true;
+  const isLoading = false;
+  const buttonAttributes = {
+    disabled: isLoading,
+    class: 'text-white bg-blue-500 hover:bg-blue-700 font-bold py-2 px-4 rounded',
+  };
+  const renderIsLoading = () => {
+    return isLoading ? 'Loading' : 'Submit';
+  };
+</script>
 
 <template>
-  <button class="btn">
+  <!-- <button :disabled="buttonDisabled" class="btn">
     <slot>BUTTON</slot>
+  </button> -->
+  <button v-bind="buttonAttributes" :id="`id-${buttonAttributes.class}`">
+    {{ renderIsLoading() }}
   </button>
 </template>
 
